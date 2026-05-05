5f55d33ddf8e64690ef3067acf0a5ae1f964102b
jakejacobs <jacky4uonly2009@gmail.com>
2026-05-05 15:08:28 +0530

    while loops

---
+++ 05-05-2026.py
@@ -0,0 +1,76 @@
+# age
+# username
+# password
+
+# first ask for age
+# then check if age is above 18
+# if age is not above 18, say 'you have to be above 18 years old'
+# if it is above 18, then you ask for username
+# if username equals 'peppy' then ask for password. or else say 'wrong username'
+# if password equals 'killer', then say, welcome or else say wrong password
+
+
+# print('Enter your age: ')
+# age = int(input())
+# if age > 18:
+#     print('enter you username')
+#     username = input()
+#     if username == 'peppy':
+#         print('Enter you password')
+#         password = input()
+#         if password == 'killer':
+#             print('welcome')
+#         else:
+#             print('wrong password')
+#     else:
+#         print('Wrong username')
+# else:
+#     print('you have to be above 18 years old.')
+
+
+# Loops
+
+# 1. while loop
+# 2. for loop
+
+# while loop
+
+# while i am in class, you should do rounds
+# but while i am not in class, you dont have todo round
+
+# if condition:
+#   code block
+
+# while condition:
+#   code block
+
+
+# if 2 > 1:
+#     print('hi')
+
+# a = 1
+# a = a + 1 #2
+# a = a + 1 #3
+# a = a + 1 #4
+
+# a = 1
+# while a <= 100:
+#     print(a)
+#     a = a + 1
+
+
+# ask for username,
+# if username equals 'peppy' then say a greeting.
+# or else ask again
+
+
+print('enter username: ')
+username = input()  # mouse
+
+while username != 'peppy':  # peppy != peppy
+    print('wrong username')
+    print('enter username again: ')
+    username = input()
+
+
+# write a while loop for numbers starting from 10 to 1.
