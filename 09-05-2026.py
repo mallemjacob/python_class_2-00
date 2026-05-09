2c6369843a726a028340eba01c36a7b5d78e219a
jakejacobs <jacky4uonly2009@gmail.com>
2026-05-09 13:32:48 +0530

    Add Guess the Number program as homework.

---
+++ 09-05-2026.py
@@ -0,0 +1,22 @@
+# Homework
+# A Short Program: Guess the Number
+
+# I am thinking of a number between 1 and 20.
+# Take a guess.
+# 10
+# Your guess is too low.
+# Take a guess.
+# 15
+# Your guess is too low.
+# Take a guess.
+# 17
+# Your guess is too high.
+# Take a guess.
+# 16
+# Good job! You guessed my number in 4 guesses!
+
+
+# Start program here
+import random
+secretNumber = random.randint(1, 20)
+print(secretNumber)
