7cfa7cf60cf5963ed99204198c03483fe8b50dc2
jakejacobs <jacky4uonly2009@gmail.com>
2026-06-08 14:54:00 +0530

    Lists - Intro

---
+++ 13.Lists.py
@@ -0,0 +1,74 @@
+
+# Lists - You can store a large numbers of related values in it.
+
+
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # length = 10
+
+# total item in nums list or length = 10
+
+# print(nums[9])
+
+# # starting index = 0
+# # endling index = length of nums - 1
+
+# animals = ['cat', 'rat', 'bat']  # length = 3
+
+
+# print(nums)
+# print(animals)
+
+# # Indexs
+# 0 - -> 1
+# 1 - -> 2
+# 2 - -> 3
+
+# print(animals[0])
+# print(animals[1])
+# print(animals[2])
+
+
+# English
+# shopping list
+
+# 0 - onions
+# 1 - bread
+# 2 - soaps
+
+# Python
+# shopping_list = ['onions', 'bread', 'soaps']
+
+# print(shopping_list)
+# print(shopping_list[1])
+
+
+# empty list
+# length = 0
+
+# newlist = []
+# print(newlist)
+
+# # list concatenantion
+
+# newlist = newlist + ['cat']
+# print(newlist)
+
+# newlist = newlist + ['bat']
+# print(newlist)
+
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(len(nums))
+# print(len(nums) - 1)
+
+# print(nums[len(nums) - 1])
+
+# print(nums[-1])
+
+# Homework
+
+
+# English - Python
+# Write a list of languages and store in a variable.
+# print the language at index 0 and at last index
+# print the length of the list
