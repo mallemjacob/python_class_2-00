b0bc5e8af6b0aa24e2a72967f52d86679be15c61
jakejacobs <jacky4uonly2009@gmail.com>
2026-06-17 15:24:38 +0530

    Add lists methods

--- lists.py
+++ lists.py
@@ -8,92 +8,239 @@
 # fruit3 = "oranges"
 
 # Index          0          1           2         3           4             5
-fruits_list = ['apples', 'bananas', 'oranges',
-               'grapes', 'strawberries', 'kiwi', 'mouse']
+# fruits_list = ['apples', 'bananas', 'oranges',
+#                'grapes', 'strawberries', 'kiwi', 'mouse']
 
 
-# Total items in fruits_list = 3
-# last index of the fruits_list = total items - 1
+# # Total items in fruits_list = 3
+# # last index of the fruits_list = total items - 1
 
-print(fruits_list)
+# print(fruits_list)
 
-print(fruits_list[0])
-print(fruits_list[1])
-print(fruits_list[2])
-print(fruits_list[3])
-
-
-print(fruits_list[4])
+# print(fruits_list[0])
+# print(fruits_list[1])
+# print(fruits_list[2])
+# print(fruits_list[3])
 
 
-print(fruits_list[len(fruits_list) - 1])  # 7 - 1
+# print(fruits_list[4])
 
+
+# print(fruits_list[len(fruits_list) - 1])  # 7 - 1
+
+# fruits_list = ['apples', 'bananas', 'oranges']
+
+# # Read a value from list
+# print(fruits_list[0])
+
+
+# # Update a value from a list
+# fruits_list[0] = 'pears'
+# print(fruits_list)
+
+
+# # Add new items to the list (list concatenation)
+# fruits_list = fruits_list + ['kiwis']
+# fruits_list = fruits_list + ['strawberries']
+# print(fruits_list)
+
+
+# # Creating a new empty list
+# animals = []
+# print(animals)
+
+# animals = animals + ['cat']  # ['cat']
+# print(animals)
+
+# animals = animals + ['dog']
+# print(animals)
+
+
+# # Delete an item from the list
+# print(fruits_list)
+# del fruits_list[0]
+
+# print(fruits_list)
+
+
+# # Accessing last item from the list
+# # Index           0         1           2
+# fruits_list = ['apples', 'bananas', 'oranges']
+# # # reverse        -3         -2          -1
+
+
+# # -3, -2, -1, 0, 1, 2, 3...
+
+# print(fruits_list)
+# print(fruits_list[2])
+# print(fruits_list[len(fruits_list) - 1])  # important
+# print(fruits_list[-1])
+
+# print(fruits_list[-2])
+# print(fruits_list[-3])
+
+# # String concatenation
+# print('hi' + 'there')
+
+# a = 9
+# a = a + 1  # a = 10
+# print(a)
+# a = a + 1  # 11
+# print(a)
+
+
+# # String index
+
+# name = 'mouse'
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+
+
+# # check if a value exists in a list
 fruits_list = ['apples', 'bananas', 'oranges']
 
-# Read a value from list
-print(fruits_list[0])
+
+# print('apples' in fruits_list)
+# print('kiwi' in fruits_list)
+
+# print('kiwi' not in fruits_list)
+# print('apples' not in fruits_list)
+
+# Find index of an item
+# print(fruits_list.index('apples'))
+# print(fruits_list.index('oranges'))
 
 
-# Update a value from a list
-fruits_list[0] = 'pears'
-print(fruits_list)
+# item_to_search = 'apples'
+
+# if item_to_search in fruits_list:
+#     print(fruits_list.index(item_to_search))
+# else:
+#     print('kiwi not in list')
 
 
-# Add new items to the list (list concatenation)
-fruits_list = fruits_list + ['kiwis']
-fruits_list = fruits_list + ['strawberries']
-print(fruits_list)
+# # Slice
+# fruits_list = ['apples', 'bananas', 'oranges',
+#                'grapes', 'strawberries', 'kiwi', 'mouse']
+
+# print(fruits_list[2:6])
 
 
-# Creating a new empty list
-animals = []
-print(animals)
+# # reverse a list
 
-animals = animals + ['cat']  # ['cat']
-print(animals)
-
-animals = animals + ['dog']
-print(animals)
+# print(fruits_list[::-1])
 
 
-# Delete an item from the list
-print(fruits_list)
-del fruits_list[0]
+# functions
+# print(), input(), str(), len(), int()
 
-print(fruits_list)
+# List methods
 
+# append() = Adds an element at the end of the list
+# animals = ['cat', 'dog', 'rat']
+# print(animals)
 
-# Accessing last item from the list
-# Index           0         1           2
-fruits_list = ['apples', 'bananas', 'oranges']
-# # reverse        -3         -2          -1
+# animals.append('mouse')
 
+# print(animals)
 
-# -3, -2, -1, 0, 1, 2, 3...
+# # clear()	= Removes all the elements from the list
+# animals.clear()
 
-print(fruits_list)
-print(fruits_list[2])
-print(fruits_list[len(fruits_list) - 1])  # important
-print(fruits_list[-1])
-
-print(fruits_list[-2])
-print(fruits_list[-3])
-
-# String concatenation
-print('hi' + 'there')
-
-a = 9
-a = a + 1  # a = 10
-print(a)
-a = a + 1  # 11
-print(a)
+# print(animals)
 
 
-# String index
+# # count() = Returns the number of elements with the specified value
+# animals = ['cat', 'dog', 'rat', 'bug', 'snake', 'dog', 'cat']
+
+# print(animals.count('cat'))
+
+
+# # extend()	= Add the elements of a list (or any iterable), to the end of the current list
+
+# a1 = [1, 2, 3]
+# a2 = [4, 5, 6]
+
+# a1.extend(a2)
+
+# print(a1)
+
+
+# # insert() = Adds an element at the specified position
+
+# animals.insert(2, 'parrot')
+
+# print(animals)
+
+
+# # pop()	= Removes the element at the specified position
+
+# print(animals)
+
+# animals.pop()
+# animals.pop(0)
+
+# print(animals)
+
+
+# # remove()	Removes the first item with the specified value
+
+# animals.remove('parrot')
+# print(animals)
+
+
+# # reverse()	= Reverses the order of the list
+
+# animals.reverse()
+
+# print(animals)
+
+
+# # sort() = Sorts the list
+
+# nums = [1, 3, 2, 5, 4, 9, 8, 7, 6]
+# animals = ['ant', 'cat', 'bug', 'elephant', 'dog']
+
+# nums.sort()
+# animals.sort()
+
+# print(nums)
+# print(animals)
+
+
+# cat list
+
+# cats = []
+
+# while True:
+#     cat_name = input('Enter a cat name:')  # snoopy
+
+#     if cat_name == '':
+#         break
+#     elif cat_name in cats:
+#         print('Name already exists. Give a new name')
+#     else:
+#         cats.append(cat_name)
+
+
+# print("The cat names are: ")
+
+# for name in cats:
+#     print(name)
+
+
+# List replication
+
+l = ['a', 'b', 'c']
+print(l * 3)
+
+
+# str to list
 
 name = 'mouse'
-print(name[0])
-print(name[1])
-print(name[2])
-print(name[3])
-print(name[4])
+name_list = list(name)
+
+print(name_list)
